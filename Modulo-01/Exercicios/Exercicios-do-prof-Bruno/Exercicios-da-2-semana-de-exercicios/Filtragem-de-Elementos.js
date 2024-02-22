f8092fbe620531336efa883410bc2e4fde2ea522
Leonardo-o-reis <149022379+LeonardoDeOliveiraReis@users.noreply.github.com>
2024-02-22 13:03:20 -0300

    #

---
+++ Modulo-01/Exercicios/Exercicios-do-prof-Bruno/Exercicios-da-2-semana-de-exercicios/Filtragem-de-Elementos.js
@@ -0,0 +1,3 @@
+let pares = [1, 2, 3, 4, 5]
+let numerosPares = pares.filter(par => par %2 ===0)
+console.log(numerosPares)
--- Modulo-01/Exercicios/Exercicios-do-prof-Bruno/Exercicios-da-2-semana-de-exercicios/Soma-de-Elementos.js
+++ Modulo-01/Exercicios/Exercicios-do-prof-Bruno/Exercicios-da-2-semana-de-exercicios/Soma-de-Elementos.js
@@ -1,3 +1,3 @@
-let numeros = [2, 5, 6 ,7, 9]
+let numeros = [1, 2, 3 ,4, 5]
 let soma = numeros.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0)
 console.log(soma)
